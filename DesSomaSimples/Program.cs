dff6842aeb934d8128046126ba86b4feaa57137e
Hiury Gomes <hiury_jp@hotmail.com>
2021-09-28 16:20:36 -0300

    DesagioSomaSimples

---
+++ DesSomaSimples/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DesSomaSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int SOMA = a + b;
+
+            Console.WriteLine("SOMA = {0}", SOMA);
+        }
+    }
+}
